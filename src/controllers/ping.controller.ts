e01dc2dd7cb219eb543c924c1413cf35e3bb1d3a
Narendra Jethi <narendrajethi220@gmail.com>
2025-06-12 00:51:49 +0530

    Added winston

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
+import logger from "../config/logger.config";
 
 export const pingHandler = async (
   req: Request,
   res: Response
 ): Promise<void> => {
+  logger.info("Ping request received");
   res.status(200).json({
     success: true,
     message: "Pong",
--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,17 @@ import { serverConfig } from "./config";
 import v1Router from "./routes/v1/index.router";
 import v2Router from "./routes/v2/index.router";
 const app = express();
+import { genericErrorHandler } from "./middlewares/error.middleware";
+
+import logger from "./config/logger.config";
+import { attachCorrelationMiddleware } from "./middlewares/correlation.middleware";
+
+app.use(attachCorrelationMiddleware);
 
 app.use("/api/v1", v1Router);
 app.use("/api/v2", v2Router);
+
+app.use(genericErrorHandler);
 
 app.listen(serverConfig.PORT, () => {
   console.log(`Server is running on ${serverConfig.PORT}`);
